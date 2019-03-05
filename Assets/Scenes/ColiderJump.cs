7da71f4a1c50d3dbf94e6cbbe539cdbed8595405
HnakadouriN <40082170+HnakadouriN@users.noreply.github.com>
2019-03-05 18:05:24 +0900

    人の挙動 (#6)
    * 人の挙動
    
    * 出来たー先輩ありがとうございます

---
+++ Assets/Scenes/ColiderJump.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColiderJump : MonoBehaviour
+{
+    private Rigidbody rb;
+    public bool key;
+    public float up;
+    public float jumpspan;
+    private float jumpbase;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        key = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* jumpbase += Time.deltaTime;
+         if(jumpbase >= jumpspan){
+             key = true;
+             jumpbase = 0;
+         }*/
+        if (Input.GetKey(KeyCode.Space) && key)
+        {
+            rb.AddForce(new Vector3(0, up, 0), ForceMode.Impulse);
+            key = false;
+        }
+    }
+    private void OnCollisionEnter(Collision othor)
+    {
+        Vector3 housen = Vector3.zero;
+
+        foreach (ContactPoint contact in othor.contacts)
+        {
+            if (contact.thisCollider.name == name)
+            {
+                housen = contact.normal;
+                Debug.Log(housen);
+                if (contact.normal.y > 0)
+                {
+                    key = true;
+                }
+            }
+
+
+
+        }
+    }
+}
---
+++ Assets/Scenes/Jikken.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jikken : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float sin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionStay(Collision other)
+    {
+        Vector3 normal = Vector3.zero;
+        //法線ベクトルの取得
+        foreach (ContactPoint contact in other.contacts)
+        {
+            if (contact.thisCollider.name == name)
+            {
+                normal = contact.normal;
+                Debug.Log(normal);
+                sin = normal.y;
+            }
+        }
+    }
+}
