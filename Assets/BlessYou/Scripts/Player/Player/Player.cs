0d6db20dd3223169fcaf312865f19736c1619c2f
Fkeito <1417089@ed.tus.ac.jp>
2019-03-09 13:15:48 +0900

    Task refactoring (#9)
    * test_bikeインポート
    
    * task_playerインポート
    
    * フォルダ整理したよ
    
    * 毎週スマブラの操作を忘れて弟と同格になります

---
+++ Assets/BlessYou/Scripts/Player/Player/Player.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speedx;
+    public float speedy;
+    public float speedz;
+    public bool jump;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        jump = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        buturi();
+	}
+    void buturi()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector3(speedx, rb.velocity.y, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector3(speedx * -1, rb.velocity.y, 0);
+        }
+        if(Input.GetKey(KeyCode.W)){
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, speedz);
+        }
+        if(Input.GetKey(KeyCode.S)){
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, speedz * -1);
+        }
+       /* if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumphantei();
+            if (jump == true)
+            {
+                rb.AddForce(new Vector3(0, speedy, 0), ForceMode.Impulse);
+                jump = false;
+            }
+        }*/
+    }
+    void jumphantei()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.distance - (0.5f * CosToTan(Vector3.Dot(Vector3.up, hit.normal)) + 0.5f) <= 0.05f)
+            {
+
+                jump = true;
+            }
+        }
+    }
+    float CosToTan(float cos){
+        return Mathf.Sqrt(1 / (cos * cos) - 1);
+    }
+}
---
+++ Assets/BlessYou/Scripts/Test/PlayerTest/Sphere.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour {
+
+
+    public Transform left;
+    public Transform right;
+    public bool re;
+    //回転時間
+    public float turn = 0.5f;
+    public GameObject hito;
+    
+	// Use this for initialization
+	void Start () {
+        hito = GameObject.Find("peple");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 center = hito.transform.position;
+        center = center + new Vector3(0, 0, 1);
+        Vector3 leftposi = left.position - center;
+        Vector3 rightposi = right.position - center;
+        Debug.Log(center);
+        if(Input.GetKey(KeyCode.A)){
+            transform.position = Vector3.Slerp(leftposi, rightposi, 2f);
+        }
+    }
+}
