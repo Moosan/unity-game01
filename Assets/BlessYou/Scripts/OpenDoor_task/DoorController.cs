4ac9abc5b5648ae9ba0569f98e6762655425302b
HnakadouriN <hasebenaoki0908@docomo.ne.jp>
2019-05-03 19:52:11 +0900

    浮くところまで！

---
+++ Assets/BlessYou/Scripts/OpenDoor_task/DoorController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    private Rigidbody door_rb;
+    public bool count;
+    private float Stoptime;
+    public float Maxtime;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        door_rb = this.gameObject.GetComponent<Rigidbody>();
+        count = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OpenDoor()
+    {
+        if (count)
+        {
+            Stoptime += Time.deltaTime;
+            door_rb.AddForce(new Vector3(0, 10, 0), ForceMode.Impulse);
+            count = false;
+            if (Stoptime > Maxtime)
+            {
+                door_rb.constraints = RigidbodyConstraints.FreezeAll;
+                Stoptime = 0;
+            }
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        count = true;
+    }
+}
---
+++ Assets/BlessYou/Scripts/OpenDoor_task/OpenDoor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    private DoorController doorCont;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject door = GameObject.Find("Door");
+        doorCont = door.GetComponent<DoorController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        doorCont.OpenDoor();
+    }
+}
