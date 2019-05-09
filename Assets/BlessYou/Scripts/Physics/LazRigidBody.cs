624a62ba118fdd0f4fda01acea614286796297ad
Moosan <washer2016@outlook.jp>
2019-05-10 05:23:29 +0900

    AddLaz

---
+++ Assets/BlessYou/Scripts/Physics/LazRigidBody.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class LazRigidBody : MonoBehaviour
+{
+    private Rigidbody Rigidbody { get; set; }
+    public float LazVolume;
+    private Vector3 Force { get; set; }
+    private void Awake()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+        Force = new Vector3(0,LazVolume,0);
+    }
+    private void FixedUpdate()
+    {
+        Rigidbody.AddForce(Force);
+    }
+    public void SetLazActive(bool isActive)
+    {
+        Force = new Vector3(0, isActive?LazVolume:0, 0);
+    }
+}
